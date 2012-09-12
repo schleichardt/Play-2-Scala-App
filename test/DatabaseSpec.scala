9403594339b6e7920ef8e30527682e9a32058181
Michael Schleichardt <michael@schleichardt.net>
2012-09-12 15:46:39 +0200

    Refactoring

--- test/DatabaseSpec.scala
+++ test/DatabaseSpec.scala
@@ -1,21 +1,26 @@
 import org.specs2.mutable._
 
+import org.specs2.specification.Example
 import play.api.db.DB
 import anorm._
 import play.api.test._
 import play.api.test.Helpers._
+import java.sql.Connection
 
 class DatabaseSpec extends Specification {
-
   "App" should {
     "be able to connect with a database" in {
-      implicit val app = FakeApplication(additionalConfiguration = inMemoryDatabase())
-      running(app) {
-        DB.withConnection { implicit c =>
-          val result: Boolean = SQL("Select 1").execute()
-          result === true
-        }
+      testWithDb { implicit c =>
+        val result: Boolean = SQL("Select 1").execute()
+        result === true
       }
+    }
+  }
+
+  def testWithDb(testBlock: Connection => Example): Example = {
+    implicit val app = FakeApplication(additionalConfiguration = inMemoryDatabase())
+    running(app) {
+      DB.withConnection { implicit c => testBlock(c) }
     }
   }
 }
